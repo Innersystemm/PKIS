137e81dd0500a15efd2d793417b3011a1e6f4b9e
= <=>
2017-12-06 20:15:02 +0300

    6 lab added

--- RestClient/target/generated-sources/cxf/wsdl/flat/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://flatservice.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package wsdl.flat;
--- RestClient/target/generated-sources/cxf/wsdl/region/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://regionservice.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package wsdl.region;
